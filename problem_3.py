dddcec5d7712750617118843915fbae260eb8ffc
Theodore Keloglou <thodoris-12@hotmail.com>
2014-06-15 02:17:23 +0300

    Solve problem 3 in Python

---
+++ problem_3.py
@@ -0,0 +1,28 @@
+# Simple function for primeness test
+def is_prime(number):
+
+  import math
+
+  limit = int(math.sqrt(number)) + 1
+
+  if number == 2:
+    return True
+  if number % 2 == 0:
+    return False
+  for i in range(3, limit, 2):
+    if number % i == 0:
+      return False
+
+  return True
+
+import math
+
+large_number = 600851475143
+limit_large_number = int(math.sqrt(large_number))
+answer = -1
+
+for i in range(2, limit_large_number):
+  if large_number % i == 0 and is_prime(i):  # short circuit evaluation FTW!
+    answer = i
+
+print("Result:", answer)
