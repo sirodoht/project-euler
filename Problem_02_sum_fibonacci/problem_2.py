486d0bb1e9f3be72e89c28440a7f31367480fa01
Theodore Keloglou <thodoris-12@hotmail.com>
2014-06-15 03:36:34 +0300

    Solve problem 2 in Python

---
+++ Problem_02_sum_fibonacci/problem_2.py
@@ -0,0 +1,25 @@
+import datetime
+
+term_a = 1
+term_b = 2
+sum = 0
+
+time_a = datetime.datetime.now()
+
+while term_a < 4000000 and term_b < 4000000:
+  print("Current Sum {:,}".format(sum))
+
+  if term_a % 2 == 0:
+    sum += term_a
+
+  if term_b % 2 == 0:
+    sum += term_b
+
+  term_a += term_b
+  term_b += term_a
+
+print("Result:", sum)
+
+time_b = datetime.datetime.now()
+
+print("Time elapsed:", time_b-time_a)
