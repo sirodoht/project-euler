f008ca3c9cd2e1df4732cd51b1d3379ff2840e16
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-06-02 21:54:32 +0300

    calculate time on problem 4 python

--- Problem_04_palindrome/problem_4.py
+++ Problem_04_palindrome/problem_4.py
@@ -1,3 +1,5 @@
+import datetime
+
 def is_palindrome(number):
     reversed_number = 0
     n = number
@@ -10,6 +12,7 @@ def is_palindrome(number):
 
     return number == reversed_number
 
+time_start = datetime.datetime.now()
 
 largest_palindrome = -1
 for i in range(100, 1000):
@@ -18,6 +21,9 @@ for i in range(100, 1000):
         product = i * j
         if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
-            print("Palindrome:", product)
+            # print("Palindrome:", product)
+
+time_end = datetime.datetime.now()
 
 print("Result:", largest_palindrome)
+print("Microseconds elapsed:", time_end - time_start)
