4cc9dbe8172deffd54e5c5650ee1fb559c650bab
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-06-02 18:10:02 +0300

    problem 1 in rust

---
+++ Problem_01_multiples_of_3_and_5/problem_1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut sum = 0;
+    
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            // println!("i = {}", i);
+            sum += i;
+        }
+    }
+
+    println!("Sum: {}", sum);
+}
