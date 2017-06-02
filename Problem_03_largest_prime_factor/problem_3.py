6f9eeb5795ca86152ab30b9d9cccfaece1c68afd
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-06-02 20:57:16 +0300

    add execution time on problem 3 python

--- Problem_03_largest_prime_factor/problem_3.py
+++ Problem_03_largest_prime_factor/problem_3.py
@@ -1,3 +1,6 @@
+import datetime
+import math
+
 # Simple function for primeness test
 def is_prime(number):
     import math
@@ -15,14 +18,17 @@ def is_prime(number):
     return True
 
 
-import math
-
 large_number = 600851475143
 limit_large_number = int(math.sqrt(large_number))
 answer = -1
+
+time_start = datetime.datetime.now()
 
 for i in range(2, limit_large_number):
     if large_number % i == 0 and is_prime(i):  # short circuit evaluation FTW!
         answer = i
 
+time_end = datetime.datetime.now()
+
 print("Result:", answer)
+print("Microseconds elapsed:", time_end - time_start)
