37bf48f4f3fe809b70d2e7a6928176ce84c3f23b
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-06-07 02:00:37 +0300

    add problem 14 in rust

---
+++ Problem_14_longest_collatz_sequence/problem_14.rs
@@ -0,0 +1,32 @@
+fn collatz(num: u32) -> u32 {
+    if num == 1 {
+        return num;
+    }
+
+    if num % 2 == 0 {
+        num / 2
+    } else {
+        (num * 3) + 1
+    }
+}
+
+fn main() {
+    let mut max_terms: u32 = 0;
+    let mut max_terms_number: u32 = 0;
+
+    for i in (2..1_000_000).rev() {
+        let mut current_terms: u32 = 0;
+        let mut result: u32 = i;
+        while result != 1 {
+            result = collatz(result);
+            current_terms += 1;
+            if current_terms > max_terms {
+                max_terms = current_terms;
+                max_terms_number = i;
+            }
+        }
+        // println!("collatz({}) -> {}", i, current_terms);
+    }
+
+    println!("Result: {}", max_terms_number);
+}
